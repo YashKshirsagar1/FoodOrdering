41db41232f842a65be0f5dd8de7d65c8e4df39fe
YashKshirsagar1 <ykshirsagar@eastsideprep.org>
2025-04-10 17:27:12 -0700

    custom component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,41 +1,13 @@
-import { StyleSheet, Image } from "react-native";
-
-import EditScreenInfo from "../../components/EditScreenInfo";
-import { Text, View } from "../../components/Themed";
-import Colors from "../../constants/Colors";
+import { View } from "../../components/Themed";
 import products from "../../../assets/data/products";
+import ProductListItem from "../../components/ProductListItem";
 
-const product = products[0];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-  <View style={styles.container}>
-    <Image source={{ uri: product.image }} style={styles.image} />
-
-    <Text style={styles.title}>{product.name}</Text>
-    <Text style={styles.price}>${product.price}</Text>
-  </View>
+    <View>
+      <ProductListItem product={products[5]}/>
+      <ProductListItem product={products[1]} />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 20, 
-    fontWeight: "bold",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-});
