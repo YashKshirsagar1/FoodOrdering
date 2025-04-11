999a3883f6eae366bc80aaf365da99c89dca01f2
YashKshirsagar1 <ykshirsagar@eastsideprep.org>
2025-04-10 18:57:56 -0700

    Type stage components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,12 @@
-import { View } from "../../components/Themed";
-import products from "../../../assets/data/products";
-import ProductListItem from "../../components/ProductListItem";
+import { View } from "@components/Themed";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListItem";
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[5]}/>
+      <ProductListItem product={products[5]} />
       <ProductListItem product={products[1]} />
     </View>
   );
 }
-
